b7b43169d2406ffda17e1243785d90dc6d3a0085
irishness11 <149637256+irishness11@users.noreply.github.com>
2025-01-20 16:12:23 -0700

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,15 +1,9 @@
 resource "aws_lb" "app_lb" {
-
   # ... other configurations ...
-
   security_groups = [aws_security_group.alb_sg.id]
-
 }
 
 resource "aws_instance" "app_instance" {
-
   # ... other configurations ...
-
   security_groups = [aws_security_group.ec2_sg.id]
-
 }
